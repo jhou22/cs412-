7bdbb0abc5783af8251f4c053569a7afc888c418
me <jhou22@bu.edu>
2024-09-30 10:35:08 -0400

    url fix

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path(r'', views.main, name="main"),
     path(r'main', views.main, name="main"),
     path(r'order', views.order, name="order"),
     path(r'confirmation', views.confirmation, name="confirmation")
