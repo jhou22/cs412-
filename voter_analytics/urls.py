24546113274980451d9474177275ae3412f0b576
me <jhou22@bu.edu>
2024-11-12 22:48:04 -0500

    a10_url

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path(r'', VotersListView.as_view(), name='voters'),
     path(r'voter/<int:pk>', VoterDetailView.as_view(), name='voter'),
-    path(r'graph', GraphView.as_view(), name='graph')
+    path(r'graphs', GraphView.as_view(), name='graphs')
 ]
